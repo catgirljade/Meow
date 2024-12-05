f4c1986619f63ad0e9a4fca5bb3f1e729d514c1d
Jade <infernoblaze326@gmail.com>
2024-12-05 22:11:44 +0800

    Initial commit

---
+++ src/main/java/org/mhealth/meow/Meow.java
@@ -0,0 +1,12 @@
+package org.mhealth.meow;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Meow implements ModInitializer {
+	/**
+	 * Runs the mod initializer.
+	 */
+	@Override
+	public void onInitialize() {
+	}
+}
